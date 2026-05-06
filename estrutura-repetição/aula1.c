a32a6379018acce9d947e1e30f40dc767a89babe
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-05-06 11:27:04 -0300

    Resumo da aula 1 sobre estrtutura de repetição
    Implement multiplication table for user input.

---
+++ estrutura-repetição/aula1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+    /*int i = 0;
+    
+    //sintaxe do for
+    //for(valor_inicial; condição de permanencia; incremento){}
+
+    for(i; i<=100; i+=2){
+        printf("%d\n", i);
+    }*/
+
+
+    //tabuada de 0 a 10 de um numero digitado por um usuario
+    int mult; 
+    int n;
+
+    //recebe o numero
+    printf("Digite um multiplo: ");
+    scanf("%d", &mult);
+
+    for(n = 0; n<=10; n++){
+        printf("%d * %d = %d\n", n, mult, n*mult);
+    }
