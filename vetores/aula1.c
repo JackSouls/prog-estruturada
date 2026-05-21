30c74deea4d906b6fb3de201156bb142baf39d3c
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-05-21 10:31:02 -0300

    Adicionado resumo da primeira aula de vetores
    Implement basic operations on a one-dimensional array.

---
+++ vetores/aula1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+    //vetores
+
+    //variaveis compostas unidemensional homogêneas
+
+    int v[5];
+
+    /*Para acessar um determinado usa essa sintaxe nome do vetor[indice], o indice vai de 0 a N-1, N sendo o tamanho do vetor.Exemplo: */
+    v[2] = 8;
+
+    //v, na posição 2, recebe o valor 8.
+    // cada posição é uma variavel comum, é possivel utilizalos para fazer operações normalmente.
+    //Exemplo:
+
+    v[3] = v[2] + 2;
+    //v, na posição 3, recebe 8 + 2, v[3] vale 10.
+
+
+}
